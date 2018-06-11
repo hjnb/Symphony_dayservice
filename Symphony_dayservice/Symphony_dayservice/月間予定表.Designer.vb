ac2edfab7a39aa91ef16a62c54ba68d390ec6761
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2018-06-11 18:07:27 +0900

    月間予定表：利用者リスト部分作成

--- Symphony_dayservice/Symphony_dayservice/月間予定表.Designer.vb
+++ Symphony_dayservice/Symphony_dayservice/月間予定表.Designer.vb
@@ -22,16 +22,41 @@ Partial Class 月間予定表
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.UserListBox = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'UserListBox
+        '
+        Me.UserListBox.FormattingEnabled = True
+        Me.UserListBox.ItemHeight = 12
+        Me.UserListBox.Location = New System.Drawing.Point(12, 31)
+        Me.UserListBox.Name = "UserListBox"
+        Me.UserListBox.Size = New System.Drawing.Size(129, 544)
+        Me.UserListBox.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(161, 131)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 349)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "追加>>"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
         '月間予定表
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(1024, 642)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.UserListBox)
         Me.Name = "月間予定表"
         Me.Text = "月間予定表"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents UserListBox As System.Windows.Forms.ListBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
 End Class
--- Symphony_dayservice/Symphony_dayservice/月間予定表.vb
+++ Symphony_dayservice/Symphony_dayservice/月間予定表.vb
@@ -1,6 +1,29 @@
-﻿Public Class 月間予定表
+﻿Imports System.Data.OleDb
+
+Public Class 月間予定表
 
     Private Sub 月間予定表_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Me.WindowState = FormWindowState.Maximized
+
+        '利用者リストの表示
+        displayUserList()
+    End Sub
+
+    Private Sub displayUserList()
+        '背景色の設定
+        UserListBox.BackColor = Color.FromKnownColor(KnownColor.Control)
+
+        'データ取得、表示
+        Dim reader As System.Data.OleDb.OleDbDataReader
+        Dim Cn As New OleDbConnection(TopForm.DB_dayservice)
+        Dim SQLCm As OleDbCommand = Cn.CreateCommand
+        SQLCm.CommandText = "select Nam from UsrM where Dsp='1' order by Kana"
+        Cn.Open()
+        reader = SQLCm.ExecuteReader()
+        While reader.Read() = True
+            UserListBox.Items.Add(Util.checkDBNullValue(reader("Nam")))
+        End While
+        reader.Close()
+        Cn.Close()
     End Sub
 End Class
