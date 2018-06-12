bcd5d155ac441132fbe0111a11c620212b20615b
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2018-06-12 17:32:27 +0900

    月間予定表：空白セル設置まで作成

--- Symphony_dayservice/Symphony_dayservice/月間予定表.Designer.vb
+++ Symphony_dayservice/Symphony_dayservice/月間予定表.Designer.vb
@@ -24,6 +24,13 @@ Partial Class 月間予定表
     Private Sub InitializeComponent()
         Me.UserListBox = New System.Windows.Forms.ListBox()
         Me.btnAdd = New System.Windows.Forms.Button()
+        Me.ymBox = New ymdBox.ymdBox()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.dgvPlan = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvPlan, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'UserListBox
@@ -45,18 +52,89 @@ Partial Class 月間予定表
         Me.btnAdd.Text = "追加>>"
         Me.btnAdd.UseVisualStyleBackColor = True
         '
+        'ymBox
+        '
+        Me.ymBox.boxType = 7
+        Me.ymBox.DateText = ""
+        Me.ymBox.EraLabelText = "H30"
+        Me.ymBox.EraText = ""
+        Me.ymBox.Location = New System.Drawing.Point(434, 6)
+        Me.ymBox.MonthLabelText = "06"
+        Me.ymBox.MonthText = ""
+        Me.ymBox.Name = "ymBox"
+        Me.ymBox.Size = New System.Drawing.Size(120, 46)
+        Me.ymBox.TabIndex = 3
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(579, 9)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(75, 40)
+        Me.btnRegist.TabIndex = 4
+        Me.btnRegist.Text = "登　録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(736, 9)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 40)
+        Me.btnPrint.TabIndex = 5
+        Me.btnPrint.Text = "印　刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(658, 9)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 40)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.Text = "削　除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(1171, 26)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(79, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "テキストクリア"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'dgvPlan
+        '
+        Me.dgvPlan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPlan.Location = New System.Drawing.Point(257, 66)
+        Me.dgvPlan.Name = "dgvPlan"
+        Me.dgvPlan.RowTemplate.Height = 21
+        Me.dgvPlan.Size = New System.Drawing.Size(993, 787)
+        Me.dgvPlan.TabIndex = 8
+        '
         '月間予定表
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1024, 642)
+        Me.ClientSize = New System.Drawing.Size(1431, 898)
+        Me.Controls.Add(Me.dgvPlan)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnRegist)
+        Me.Controls.Add(Me.ymBox)
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.UserListBox)
         Me.Name = "月間予定表"
         Me.Text = "月間予定表"
+        CType(Me.dgvPlan, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents UserListBox As System.Windows.Forms.ListBox
     Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents ymBox As ymdBox.ymdBox
+    Friend WithEvents btnRegist As System.Windows.Forms.Button
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents dgvPlan As System.Windows.Forms.DataGridView
 End Class
--- Symphony_dayservice/Symphony_dayservice/月間予定表.vb
+++ Symphony_dayservice/Symphony_dayservice/月間予定表.vb
@@ -2,11 +2,55 @@
 
 Public Class 月間予定表
 
+    '土曜日の列のセルスタイル
+    Private saturdayColumnCellStyle As DataGridViewCellStyle
+
+    '日曜日の列のセルスタイル
+    Private sundayColumnCellStyle As DataGridViewCellStyle
+
+    '日にちの行のセルスタイル
+    Private dateRowCellStyle As DataGridViewCellStyle
+
+    '曜日の行のセルスタイル
+    Private dayRowCellStyle As DataGridViewCellStyle
+
+    '
+    Private dt As DataTable = New DataTable()
+
     Private Sub 月間予定表_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Me.WindowState = FormWindowState.Maximized
 
         '利用者リストの表示
         displayUserList()
+
+        '
+        initDgvPlan()
+
+        '現在年月設定
+        ymBox.setADStr(Today.ToString("yyyy/MM/dd"))
+
+    End Sub
+
+    Private Sub createCellStyle()
+        '日にちの行のセルスタイル
+        dateRowCellStyle = New DataGridViewCellStyle()
+        dateRowCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        dateRowCellStyle.Font = New Font("MS UI Gothic", 9)
+
+        '曜日の行のセルスタイル
+        dayRowCellStyle = New DataGridViewCellStyle()
+        dayRowCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        dayRowCellStyle.Font = New Font("MS UI Gothic", 9)
+
+        '土曜日の列のセルスタイル
+        saturdayColumnCellStyle = New DataGridViewCellStyle()
+        saturdayColumnCellStyle.BackColor = Color.FromArgb(200, 200, 255)
+        saturdayColumnCellStyle.Font = New Font("MS UI Gothic", 8)
+
+        '日曜日の列のセルスタイル
+        sundayColumnCellStyle = New DataGridViewCellStyle()
+        sundayColumnCellStyle.BackColor = Color.FromArgb(255, 200, 200)
+        sundayColumnCellStyle.Font = New Font("MS UI Gothic", 8)
     End Sub
 
     Private Sub displayUserList()
@@ -26,4 +70,74 @@ Public Class 月間予定表
         reader.Close()
         Cn.Close()
     End Sub
+
+    Private Sub settingDgvPlan()
+        'DoubleBufferedプロパティをTrue
+        Util.EnableDoubleBuffering(dgvPlan)
+
+        'dgv設定
+        With dgvPlan
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False
+            .MultiSelect = False
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .RowHeadersVisible = False
+            .ColumnHeadersVisible = False
+            .RowTemplate.Height = 14
+            .ShowCellToolTips = False
+            .DefaultCellStyle.Font = New Font("MS UI Gothic", 8)
+        End With
+    End Sub
+
+    Private Sub settingDgvPlanColumn()
+        With dgvPlan
+            With .Columns("Count")
+                .Width = 30
+            End With
+
+            For i As Integer = 1 To 16
+                With .Columns("D" & i)
+                    .Width = 60
+                End With
+            Next
+        End With
+    End Sub
+
+    Private Sub initDgvPlan()
+        'dgv設定
+        settingDgvPlan()
+
+        '列追加
+        dt.Columns.Add("Count", Type.GetType("System.String"))
+        For i As Integer = 1 To 16
+            dt.Columns.Add("D" & i, Type.GetType("System.String"))
+        Next
+
+        '日にちの行、空の行追加
+        Dim row As DataRow
+        For i As Integer = 0 To 55
+            row = dt.NewRow()
+            If i = 0 Then
+                For j As Integer = 1 To 16
+                    row("D" & j) = j
+                Next
+            ElseIf i = 28 Then
+                For j As Integer = 1 To 15
+                    row("D" & j) = j + 16
+                Next
+            End If
+            dt.Rows.Add(row)
+        Next
+
+        '表示
+        dgvPlan.DataSource = dt
+
+        '列設定等
+        settingDgvPlanColumn()
+
+    End Sub
+
 End Class
