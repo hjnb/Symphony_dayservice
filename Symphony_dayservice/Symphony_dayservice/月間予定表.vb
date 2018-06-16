3b22044057ecfe3a62694f218936b0e486767a9e
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2018-06-16 12:33:20 +0900

    月間予定表：スタイル作成等

--- Symphony_dayservice/Symphony_dayservice/月間予定表.vb
+++ Symphony_dayservice/Symphony_dayservice/月間予定表.vb
@@ -14,7 +14,22 @@ Public Class 月間予定表
     '曜日の行のセルスタイル
     Private dayRowCellStyle As DataGridViewCellStyle
 
-    '
+    'Count列の5までのセルスタイル
+    Private count5CellStyle As DataGridViewCellStyle
+
+    'Count列の10までのセルスタイル
+    Private count10CellStyle As DataGridViewCellStyle
+
+    'Count列の15までのセルスタイル
+    Private count15CellStyle As DataGridViewCellStyle
+
+    'Count列の20までのセルスタイル
+    Private count20CellStyle As DataGridViewCellStyle
+
+    'Count列の25までのセルスタイル
+    Private count25CellStyle As DataGridViewCellStyle
+
+    '表示用データテーブル
     Private dt As DataTable = New DataTable()
 
     Private Sub 月間予定表_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
@@ -55,6 +70,37 @@ Public Class 月間予定表
         sundayColumnCellStyle = New DataGridViewCellStyle()
         sundayColumnCellStyle.BackColor = Color.FromArgb(255, 200, 200)
         sundayColumnCellStyle.Font = New Font("MS UI Gothic", 8)
+
+        'Count列の5までのセルスタイル
+        count5CellStyle = New DataGridViewCellStyle()
+        count5CellStyle.BackColor = Color.FromArgb(254, 222, 253)
+        count5CellStyle.Font = New Font("MS UI Gothic", 8)
+        count5CellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        'Count列の10までのセルスタイル
+        count10CellStyle = New DataGridViewCellStyle()
+        count10CellStyle.BackColor = Color.FromArgb(248, 154, 245)
+        count10CellStyle.Font = New Font("MS UI Gothic", 8)
+        count10CellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        'Count列の15までのセルスタイル
+        count15CellStyle = New DataGridViewCellStyle()
+        count15CellStyle.BackColor = Color.FromArgb(233, 31, 233)
+        count15CellStyle.Font = New Font("MS UI Gothic", 8)
+        count15CellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        'Count列の20までのセルスタイル
+        count20CellStyle = New DataGridViewCellStyle()
+        count20CellStyle.BackColor = Color.FromArgb(194, 18, 172)
+        count20CellStyle.Font = New Font("MS UI Gothic", 8)
+        count20CellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        'Count列の25までのセルスタイル
+        count25CellStyle = New DataGridViewCellStyle()
+        count25CellStyle.BackColor = Color.FromArgb(255, 200, 200)
+        count25CellStyle.Font = New Font("MS UI Gothic", 8)
+        count25CellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
     End Sub
 
     Private Sub displayUserList()
@@ -117,6 +163,11 @@ Public Class 月間予定表
         dgvPlan.Rows(1).DefaultCellStyle = dayRowCellStyle
         dgvPlan.Rows(29).DefaultCellStyle = dayRowCellStyle
 
+        'Count列の5までのセルスタイル
+        For i As Integer = 2 To 6
+            dgvPlan.Rows(i).Cells("Count").Style = count5CellStyle
+        Next
+
     End Sub
 
     Private Sub initDgvPlan()
@@ -138,11 +189,17 @@ Public Class 月間予定表
                 For j As Integer = 1 To 16
                     row("D" & j) = j
                 Next
+            ElseIf i > 1 AndAlso i < 28 AndAlso (i - 1) Mod 5 = 0 Then
+                row("Count") = i - 1
+            ElseIf i = 27 OrElse i = 55 Then
+                row("Count") = "計"
             ElseIf i = 28 Then
                 '日にち(17～31)
                 For j As Integer = 1 To 15
                     row("D" & j) = j + 16
                 Next
+            ElseIf i > 29 AndAlso (i + 1) Mod 5 = 0 Then
+                row("Count") = i - 30 + 1
             End If
             dt.Rows.Add(row)
         Next
